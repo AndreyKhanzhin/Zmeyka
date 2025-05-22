78eef091f45a34db1fc1d128b904a0c8c7b855b1
AndreyKhanzhin <118679620+AndreyKhanzhin@users.noreply.github.com>
2025-05-22 12:58:15 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿Console.WriteLine("Введите размерность матрицы");
+
+int n;
+while (!(int.TryParse(Console.ReadLine(), out n))) {
+    Console.WriteLine("Вы ввели не те данные.");
+}
+
+int[,] matrix = new int[n, n];
+int num = 1;
+
+for (int d = 0; d < 2 * n - 1; d++) {
+    
+    int r;
+
+    if (d < n) {
+        r = 0;
+    } else { 
+        r = d - n + 1;
+    }
+    
+    int c;
+    
+    if (d < n) { 
+        c = d;
+    }
+    else { 
+        c = n - 1;
+    }
+
+    if (d % 2 == 0) {
+        for (int i = r, j = c; i < n && j >= 0; i++, j--) {
+            matrix[i, j] = num++;
+        }
+    }
+    else {
+        for (int i = c, j = r; i >= 0 && j < n; i--, j++) {
+            matrix[i, j] = num++;
+        }
+    }
+}
+
+for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+        Console.Write($"{matrix[i, j],4}");
+    }
+    Console.WriteLine();
+}
